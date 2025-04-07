206afb13a717135219f66f481c2a29468889017b
Eric Van Clepper <ericvc@uchicago.edu>
2025-04-07 10:24:59 -0500

    Include hydrogen mass and boltzmann constants

--- pywrapper/constants.py
+++ pywrapper/constants.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+MH = 1.66053907e-24
+BK = 1.38e-16 # Boltzmann constant [cgs]
 MSUN = 1.9891e33
 MEARTH = 5.97e27
 AU = 1.49597871e13
@@ -7,7 +9,7 @@ G = 6.674e-8
 YR = 3.1557600e7
 RJUP = 6.995e9
 
-R0 = ( 5.2*AU )
+R0 = ( 75*AU )
 
 LEN = ( R0 )
 MASS = ( MSUN )
